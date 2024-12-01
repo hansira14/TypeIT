ec6f62846b58a5533bc83ab15810e6c1be1c7319
hansira14 <irahansdedicatoria@gmail.com>
2024-12-01 17:29:44 +0800

    Add new page for profile name input

---
+++ TypeIT/Forms/InputProfileName.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace TypeIT.Forms
+{
+    partial class InputProfileName
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            cancelProfile = new Guna.UI2.WinForms.Guna2Button();
+            guna2TextBox2 = new Guna.UI2.WinForms.Guna2TextBox();
+            profileName = new Guna.UI2.WinForms.Guna2TextBox();
+            saveProfileName = new Guna.UI2.WinForms.Guna2Button();
+            SuspendLayout();
+            // 
+            // cancelProfile
+            // 
+            cancelProfile.BackColor = Color.Transparent;
+            cancelProfile.BorderRadius = 4;
+            cancelProfile.CustomizableEdges = customizableEdges9;
+            cancelProfile.DisabledState.BorderColor = Color.DarkGray;
+            cancelProfile.DisabledState.CustomBorderColor = Color.DarkGray;
+            cancelProfile.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            cancelProfile.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            cancelProfile.FillColor = Color.Transparent;
+            cancelProfile.Font = new Font("Segoe UI", 9F);
+            cancelProfile.ForeColor = Color.White;
+            cancelProfile.Location = new Point(191, 328);
+            cancelProfile.Margin = new Padding(6);
+            cancelProfile.Name = "cancelProfile";
+            cancelProfile.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            cancelProfile.Size = new Size(147, 79);
+            cancelProfile.TabIndex = 17;
+            cancelProfile.Text = "Cancel";
+            // 
+            // guna2TextBox2
+            // 
+            guna2TextBox2.BackColor = Color.Transparent;
+            guna2TextBox2.BorderColor = Color.Transparent;
+            guna2TextBox2.BorderThickness = 0;
+            guna2TextBox2.CustomizableEdges = customizableEdges11;
+            guna2TextBox2.DefaultText = "Input name of new profile";
+            guna2TextBox2.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            guna2TextBox2.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            guna2TextBox2.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox2.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox2.FillColor = Color.FromArgb(10, 10, 12);
+            guna2TextBox2.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox2.Font = new Font("Segoe UI", 9F);
+            guna2TextBox2.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox2.Location = new Point(85, 70);
+            guna2TextBox2.Margin = new Padding(11, 13, 11, 13);
+            guna2TextBox2.Multiline = true;
+            guna2TextBox2.Name = "guna2TextBox2";
+            guna2TextBox2.PasswordChar = '\0';
+            guna2TextBox2.PlaceholderText = "";
+            guna2TextBox2.ReadOnly = true;
+            guna2TextBox2.SelectedText = "";
+            guna2TextBox2.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            guna2TextBox2.Size = new Size(322, 69);
+            guna2TextBox2.TabIndex = 16;
+            // 
+            // profileName
+            // 
+            profileName.BackColor = Color.Transparent;
+            profileName.BorderColor = Color.Transparent;
+            profileName.BorderRadius = 6;
+            profileName.CustomizableEdges = customizableEdges13;
+            profileName.DefaultText = "";
+            profileName.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            profileName.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            profileName.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            profileName.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            profileName.FillColor = Color.FromArgb(16, 17, 19);
+            profileName.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            profileName.Font = new Font("Segoe UI", 9F);
+            profileName.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            profileName.Location = new Point(85, 154);
+            profileName.Margin = new Padding(11, 13, 11, 13);
+            profileName.Name = "profileName";
+            profileName.PasswordChar = '\0';
+            profileName.PlaceholderText = "";
+            profileName.SelectedText = "";
+            profileName.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            profileName.Size = new Size(421, 128);
+            profileName.TabIndex = 15;
+            // 
+            // saveProfileName
+            // 
+            saveProfileName.BorderRadius = 4;
+            saveProfileName.CustomizableEdges = customizableEdges15;
+            saveProfileName.DisabledState.BorderColor = Color.DarkGray;
+            saveProfileName.DisabledState.CustomBorderColor = Color.DarkGray;
+            saveProfileName.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            saveProfileName.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            saveProfileName.Font = new Font("Segoe UI", 9F);
+            saveProfileName.ForeColor = Color.White;
+            saveProfileName.Location = new Point(359, 328);
+            saveProfileName.Margin = new Padding(6);
+            saveProfileName.Name = "saveProfileName";
+            saveProfileName.ShadowDecoration.CustomizableEdges = customizableEdges16;
+            saveProfileName.Size = new Size(147, 79);
+            saveProfileName.TabIndex = 18;
+            saveProfileName.Text = "Next";
+            // 
+            // InputProfileName
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(605, 523);
+            Controls.Add(saveProfileName);
+            Controls.Add(cancelProfile);
+            Controls.Add(guna2TextBox2);
+            Controls.Add(profileName);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "InputProfileName";
+            ShowIcon = false;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button cancelProfile;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox2;
+        private Guna.UI2.WinForms.Guna2TextBox profileName;
+        private Guna.UI2.WinForms.Guna2Button saveProfileName;
+    }
+}
